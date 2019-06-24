516ab76626c0db68975854344533b9669126f2dc
Raiu1210 <baio1484@gmail.com>
2019-06-24 12:47:53 +0900

    Initial Commit

---
+++ open_other_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  open_other_app
+//
+//  Created by Ryu Ishibashi on 2019/06/24.
+//  Copyright © 2019 Ryu Ishibashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
